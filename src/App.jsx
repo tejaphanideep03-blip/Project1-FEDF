b4032d043baa3f19029f86176a22e9c398319034
tejaphanideep03-blip <tejaphanideep03@gmail.com>
2026-01-02 10:10:06 +0530

    v1

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='app'>
+      <h1>My Profile</h1>
+      <p>Welcome to my First React application.</p>
+      <table>
+        <tr>
+              <th>Name</th>
+              <td>Teja Phanideep</td>
+          </tr>
+          <tr>
+              <th>Section</th>
+              <td>03</td>
+          </tr>
+          <tr>
+              <th>Id Number</th>
+              <td>2500031314</td>
+          </tr>
+          <tr>
+              <th>Branch</th>
+              <td>CSE</td>
+          </tr>
+          <tr>
+              <th>Program</th>
+              <td>B.Tech</td>
+          </tr>
+          <tr>
+              <th>Course</th>
+              <td>Frontend Web Development and UI Engineering</td>
+          </tr>
+        </table>
+    </div>
+  );
+}
+export default App;
